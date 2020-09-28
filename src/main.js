e9724553b22dd0b2c1afb12bf054f18b08c23724
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-27 18:07:05 -0700

    API is called.

--- src/main.js
+++ src/main.js
@@ -1,33 +1,27 @@
-import $ from 'jquery'
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function() {
-
-  $('#AUD').click(function() {
-    let request = new XMLHttpRequest();
-    const url = 'https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD'
-  }
-
-
-
-
+function convertAUD(response) {
+  $('.showAUD').text(response.conversion_rates.AUD);
 }
 
+$(document).ready(function() {
+  $('#submitDollar').click(function() {
+
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/61905536dd0b5c15fe86d220/latest/USD`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        convertAUD(response);
+      }
+    };
+    request.open("GET", url, true);
+    request.send();
 
 
-
-$('#get-date').click(function() {
-  let request = new XMLHttpRequest();
-  const url = `https://api.nasa.gov/insight_weather/?api_key=${process.env.API_KEY}&feedtype=json&ver=1.0`
-  request.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      const response = JSON.parse(this.responseText);
-      displayData(response);
-    }
-  };  
-  request.open("GET", url, true);
-  request.send();
-  $('#data').toggle();
-})  
+  });
+});
